098b41d1fbbd198ffcbbe3b951128e1c8e359e1f
Todd DeLuca <nospam@todddeluca.com>
2013-01-31 15:57:17 -0500

    Add a python version of unix which command

--- tfd/util.py
+++ tfd/util.py
@@ -28,6 +28,27 @@ def coroutine(func):
     return start
 
 
+def which(program):
+    '''
+    Emulate unix 'which' command.
+    http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python/377028#377028
+    '''
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
+
 ######################
 # WORKING WITH NUMBERS
 
