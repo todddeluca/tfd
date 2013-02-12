3d1862c125f40c0eac83c5aca10797a27f9aabf8
Todd DeLuca <nospam@todddeluca.com>
2013-02-12 17:25:01 -0500

    Add mlsd, username and password params. Remove silly parse_netloc.
    It turns out that urlparse does everything and more that
    parse_netloc does.  So remove parse_netloc.
    
    Add a function to make an MLSD query and parse the results.
    
    Add parameters so a user can specify the password and username for
    logging in to an ftp server.

--- tfd/ftputil.py
+++ tfd/ftputil.py
@@ -6,46 +6,8 @@ import time
 import urlparse
 
 
-def parse_netloc(netloc):
-    '''
-    Parse out and return a tuple of user, password, host, port from a network
-    location.  Port is optional.  User is optional (but required if a password
-    is given).  Password is optional.  Any missing components will be None
-    in the returned tuple.  Note, if port is present, it is returned as an
-    integer.
-
-    netloc: In the form '[user[:password]@]host[:port]'
-    '''
-    username = password = host = port = None
-
-    if '@' in netloc:
-        userpass, hostport = netloc.split('@', 1)
-    else:
-        userpass = None
-        hostport = netloc
-
-    if userpass is not None:
-        if ':' in userpass:
-            username, password = userpass.split(':', 1)
-        else:
-            username = userpass
-
-    if ':' in hostport:
-        host, port = hostport.split(':', 1)
-        try:
-            port = int(port)
-        except ValueError:
-            raise Exception('Port is not an integer.', port)
-        if port < 0:
-            raise Exception('Port must be >= 0.', port)
-    else:
-        host = hostport
-
-    return username, password, host, port
-
-
 @contextlib.contextmanager
-def connect_and_login(url, ftp=None):
+def connect_and_login(url, ftp=None, username=None, password=None):
     '''
     Connect to and login to an ftp server and yield the FTP object.  The
     ftp connection will be closed after the yield statement.
@@ -59,11 +21,9 @@ def connect_and_login(url, ftp=None):
         yield ftp
     else:
         # extract host and port
-        # scheme://netloc/path;parameters?query#fragment
-        scheme, netloc, path, query, fragment = urlparse.urlsplit(url)
-        # username:password@host:port -> host
-        username, password, host, port = parse_netloc(netloc)
-
+        result = urlparse.urlsplit(url)
+        host = result.hostname
+        port = result.port
         # connect
         ftp = ftplib.FTP()
         if port is None:
@@ -85,7 +45,7 @@ def connect_and_login(url, ftp=None):
 
 
 
-def walk(url, depth=-1, conn=None, pause=0):
+def walk(url, depth=-1, conn=None, pause=0, username=None, password=None):
     '''
     Traverse a directory tree rooted at url.  Similar to os.walk.  Walks the
     directories (in a depth-first manner) starting at url, yielding tuples of
@@ -104,7 +64,8 @@ def walk(url, depth=-1, conn=None, pause=0):
     search only <depth> levels beneath the current dir.  If depth is 0, only
     yield the files in the directory specified by url.
     :param conn: An open, logged in ftp connection.  If None, a connection to
-    url will be opened (and closed).
+    url will be opened (and closed).  Conn should be open to the same host
+    as the hostname in url.
     :param pause: How long to pause between listing each directory.  Defaults
     to no pause.  This is useful for throttling requests to the server.
 
@@ -116,7 +77,7 @@ def walk(url, depth=-1, conn=None, pause=0):
         'SeriesMatrix', 'annotation', 'projects', 'roadmapepigenomics',
         'supplementary'], ['datasets_gi.txt.gz'])]
     '''
-    with connect_and_login(url, ftp=conn) as ftp:
+    with connect_and_login(url, ftp=conn, username=username, password=password) as ftp:
         url, dirs, files = listdir(url, conn=ftp)
         yield url, dirs, files
         # throttle ftp server requests
@@ -132,16 +93,21 @@ def walk(url, depth=-1, conn=None, pause=0):
                 yield retval
 
 
-def isdir(url, conn=None):
+def isdir(url, conn=None, username=None, password=None):
     '''
     Return True or False depending on whether or not the path component of the
     url refers to a directory (as determined by CWD <path>) on the ftp server
     the url refers to.
+
+    :param url: An valid ftp url pointing to a directory.
+    :param conn: An open, logged in ftp connection.  If None, a connection to
+    url will be opened (and closed).  Conn should be open to the same host
+    as the hostname in url.
     '''
     # get path from url
     scheme, netloc, path, query, fragment = urlparse.urlsplit(url)
 
-    with connect_and_login(url, ftp=conn) as ftp:
+    with connect_and_login(url, ftp=conn, username=username, password=password) as ftp:
         try:
             ftp.cwd(path)
             return True
@@ -156,11 +122,80 @@ def isdir(url, conn=None):
             else:
                 raise
 
-def listdir(url, conn=None):
+
+def mlsd(url, conn=None, use_fact_dict=True, username=None, password=None):
+    '''
+    Change to the directory indicated by URL, list the directory using MLSD,
+    and return a list, where each entry in the list is a tuple of the pathname
+    returned by the MLSD command and the facts.  By default, the facts entry is
+    a dict containing the keywords (lowercased) and values returned by the MLSD
+    command.  If use_fact_dict is False, the facts entry is a list of tuples
+    containing the keywords (lowercased) and values returned by the MLSD
+    command.  This option exists to handle the case where multiple instances of
+    the same keyword are returned by the MLSD command.
+
+    :param url: An valid ftp url pointing to a directory.
+    :param conn: An open, logged in ftp connection.  If None, a connection to
+    url will be opened (and closed).  Conn should be open to the same host
+    as the hostname in url.
+
+    Example:
+
+        mlsd("ftp://ftp.ncbi.nlm.nih.gov/pub/geo/")'
+        [('.', {'unix.owner': '14', 'unix.mode': '0444', 'modify':
+                '20120206193711', 'perm': 'fle', 'unix.group': '5007',
+                'unique': '17U7D7C6', 'type': 'cdir', 'size': '4096'}),
+         ('..', {'unix.owner': '14', 'unix.mode': '0444', 'modify':
+                 '20130207193122', 'perm': 'fle', 'unix.group': '0',
+                 'unique': '17U1080BC', 'type': 'pdir', 'size': '8192'}),
+         ('DATA', {'unix.owner': '14', 'unix.mode': '0444', 'modify':
+                   '20130212111247', 'perm': 'fle', 'unix.group':
+                   '0', 'unique': '41U2', 'type': 'dir', 'size': '0'}),
+         ('README.TXT', {'unix.owner': '14', 'unix.mode': '0444', 'modify':
+                         '20100923150800', 'perm': 'adfr', 'unix.group':
+                         '5007', 'unique': '17U7B62C', 'type': 'file',
+                         'size': '7254'})]
+    '''
+    # A bit about parsing MLSD lines
+    # http://tools.ietf.org/html/rfc3659#section-7
+    # The set of facts must not contain any spaces anywhere inside it.
+    # The facts are a series of keyword=value pairs each followed by semi-colon
+    # (";") characters.  An individual fact may not contain a semi-colon in its
+    # name or value.  The complete series of facts may not contain the space
+    # character.
+
+    # get path from url
+    scheme, netloc, path, query, fragment = urlparse.urlsplit(url)
+    output = []
+    def parse(line):
+        # print line
+        fact_list = []
+        facts_str, pathname = line.split(' ', 1)
+        # print 'facts_str, pathname =', facts_str, pathname
+        while facts_str:
+            fact, facts_str = facts_str.split(';', 1)
+            keyword, value = fact.split('=')
+            # print 'keyword, value =', keyword, value
+            fact_list.append((keyword.lower(), value))
+            # print 'added fact'
+        facts = dict(fact_list) if use_fact_dict else fact_list
+        output.append((pathname.strip(), facts))
+        # print 'fact, facts_str =', fact, facts_str
+
+    with connect_and_login(url, ftp=conn, username=username, password=password) as ftp:
+        ftp.cwd(path)
+        ftp.retrlines('MLSD', parse)
+
+    return output
+
+
+
+def listdir(url, conn=None, username=None, password=None):
     '''
     :param url: An valid ftp url pointing to a directory.
     :param conn: An open, logged in ftp connection.  If None, a connection to
-    url will be opened (and closed).
+    url will be opened (and closed).  Conn should be open to the same host
+    as the hostname in url.
 
     Example:
 
@@ -172,13 +207,12 @@ def listdir(url, conn=None):
     # get path from url
     scheme, netloc, path, query, fragment = urlparse.urlsplit(url)
 
-    with connect_and_login(url, ftp=conn) as ftp:
+    with connect_and_login(url, ftp=conn, username=username, password=password) as ftp:
         ftp.cwd(path)
         files = []
         dirs = []
         ftp.retrlines('LIST', _gather_dirs_and_files(dirs, files))
         return (url, dirs, files)
-
 
 
 def _gather_dirs_and_files(dirs, files):
